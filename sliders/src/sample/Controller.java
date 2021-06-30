c26adb457d082c8d38b99ee3522feb92bdb3a892
viking736 <viking25082000@gmail.com>
2021-06-30 13:35:58 +0300

    basics of sliders in JavaFX

---
+++ sliders/src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Slider mySlider;
+    @FXML
+    private Label myText;
+
+    int temperature;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        temperature = (int) mySlider.getValue();
+        myText.setText(temperature + "°\u200BC");
+        mySlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                temperature = (int) mySlider.getValue();
+                myText.setText(temperature + "°\u200BC");
+            }
+        });
+    }
+}
