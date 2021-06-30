fb80a5777530a49948a8aa1fcdf1c0fb3ff6e1c1
viking736 <viking25082000@gmail.com>
2021-06-30 18:18:03 +0300

    basics of keyevents in JavaFX

---
+++ keyEvents/src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Controller {
+
+    public void moveUp(){
+        System.out.println("Moving up!");
+    }
+    public void moveDown(){
+        System.out.println("Moving down!");
+    }
+    public void moveRight(){
+        System.out.println("Moving right!");
+    }
+    public void moveLeft(){
+        System.out.println("Moving left!");
+    }
+}
---
+++ keyEvents/src/sample/Main.java
@@ -0,0 +1,51 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()){
+                    case UP:
+                        controller.moveUp();
+                        break;
+                    case DOWN:
+                        controller.moveDown();
+                        break;
+                    case RIGHT:
+                        controller.moveRight();
+                        break;
+                    case LEFT:
+                        controller.moveLeft();
+                        break;
+                    default:
+                        System.out.println("Unrecognizable command");
+                }
+            }
+        });
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
